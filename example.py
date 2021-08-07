e4c4bf49d56ab002137973ad6a395bb660bc2fa4
thissop <68932815+thissop@users.noreply.github.com>
2021-08-07 15:10:19 -0700

    Create example.py

---
+++ example.py
@@ -0,0 +1,23 @@
+def sokolovskyNu(x,xerr):
+    '''
+    Best working version for calculating nu variabilty metric 
+    from Sokolovsky et al. 2017. 
+    '''
+    
+    # Import(s)
+    import numpy as np
+    
+    # Action
+    # Equation: [ (m-e)max - (m+e)min ] / [ (m-e)max + (m+e)min ]
+
+    x = np.array(x)
+    xerr = np.array(xerr)
+    
+    x_minus_xerr = x-xerr
+    x_plus_xerr = x+xerr
+    
+    min_val = np.amin(x_plus_xerr)
+    max_val = np.amax(x_minus_xerr)
+    
+    nu = (max_val-min_val)/(max_val+min_val)
+    return nu

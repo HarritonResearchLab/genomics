3a361c8035aaa07413fe30a123f16151d0b83496
Noah <Braunfeld>
2021-11-24 20:01:34 -0500

    minor tweaks to formatting of output [prettified:rainbow:]

--- exploring/first_topics/gene_prediction/tissue_prediction/noah_attempt.py
+++ exploring/first_topics/gene_prediction/tissue_prediction/noah_attempt.py
@@ -10,17 +10,10 @@ tissue_df = pd.read_csv('data/tissue_df.csv')
 outcomes_df = pd.read_csv('data/outcomes_df.csv')
 
 gene_df = pd.read_feather('data/genes_only.ftr')
-tissue_classes = {'cerebellum':0, 
-                   'placenta':1,
-                   'kidney':2, 
-                   'endometrium':3,
-                   'liver':4,
-                   'colon':5,
-                   'hippocampus':6}
 
 mds = MDS()
 X=mds.fit_transform(gene_df)
 model = KNeighborsClassifier()
 X_train, X_test, y_train, y_test = train_test_split(X, tissue_df['tissue_ordinal'], test_size=0.30)
 model.fit(X_train, y_train)
-print(accuracy_score(model.predict(X_test), y_test))
+print(f"Accuracy: {accuracy_score(model.predict(X_test), y_test)*100:.2f}%")
